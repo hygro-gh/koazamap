33695290d84180444e6748104ed5744f2b102dc6
hygro-gh <kenfou@gmail.com>
2025-01-03 23:43:46 +0900

    Add files via upload

--- styles/Villege_marge_6_style.js
+++ styles/Villege_marge_6_style.js
@@ -12,7 +12,7 @@ var style_Villege_marge_6 = function(feature, resolution){
     var labelFill = "#003bfe";
     var bufferColor = "#fafafa";
     var bufferWidth = 2.0;
-    var textAlign = "left";
+    var textAlign = "center";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
@@ -22,7 +22,7 @@ var style_Villege_marge_6 = function(feature, resolution){
     var style = [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: 'rgba(0,59,254,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
+                              labelFill, placement, bufferColor, textAlign,
                               bufferWidth)
     })];
 
