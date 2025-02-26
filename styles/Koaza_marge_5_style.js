e796981343257eee38de179412d663b3d14694cb
hygro-gh <kenfou@gmail.com>
2025-02-27 00:48:04 +0900

    Add files via upload

--- styles/Koaza_marge_5_style.js
+++ styles/Koaza_marge_5_style.js
@@ -8,7 +8,7 @@ var style_Koaza_marge_5 = function(feature, resolution){
     };
     
     var labelText = ""; var value = feature.get("");
-    var labelFont = "14px \'Osaka\', sans-serif";
+    var labelFont = "12px \'Osaka\', sans-serif";
     var labelFill = "#e41a1c";
     var bufferColor = "";
     var bufferWidth = 0;
